586e7545470abb5311c450aacc036e9eddb2b993
root <richkibz@gmail.com>
2023-02-10 12:04:10 +0300

    user attributes

---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module creates a user class"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ inherits from basemodel """
+    
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+

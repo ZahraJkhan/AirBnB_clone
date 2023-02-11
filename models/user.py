7d85e0593555762f76a15bf7bd1c5f64377fc69b
root <richkibz@gmail.com>
2023-02-11 10:46:06 +0300

    fixed user class

--- models/user.py
+++ models/user.py
@@ -6,9 +6,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """ inherits from basemodel """
-    
     email = ""
     password = ""
     first_name = ""
     last_name = ""
-
